e1905da09f4cf9505318bb1fa8ad5433beb42b2b
Eugene Seager <eugene.seager@gmail.com>
2020-02-11 14:30:03 +0300

    Try to improve speed

--- src/main.py
+++ src/main.py
@@ -9,22 +9,26 @@ webcam_angle = math.pi/3
 
 def main():
     window_name = "Face tracking"
-    vs = VideoStream(src=1).start()
+    vs = VideoStream(src=0).start()
     cv2.namedWindow(window_name, cv2.WINDOW_GUI_EXPANDED)
     face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
     face_holder = FaceHolder()
 
-    with ThreadPoolExecutor(max_workers=2) as executor:
+    with ThreadPoolExecutor(max_workers=4) as executor:
         while True:
             frame = vs.read()
-            faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=8, minSize=(10, 10))
-            face, centroid = face_holder.update(faces)  # actual face
+            faces = executor.submit(proceed_frame, face_cascade, frame)
+            face, centroid = face_holder.update(faces.result())  # actual face
             if face is not None:
                 (x, y, w, h) = face
                 executor.submit(calculate_angle, centroid[0], frame.shape[1], webcam_angle)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
             cv2.imshow(window_name, frame)
             cv2.waitKey(1)
+
+
+def proceed_frame(face_cascade, frame):
+    return face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=8, minSize=(10, 10))
 
 
 def calculate_angle(centroid_x, image_width, camera_angle):
